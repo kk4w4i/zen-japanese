374df879a5dbe6601f533438809dcfb8db91c376
kk4w4i <kintarokawai@gmail.com>
2024-04-17 17:35:34 +1000

    added typeface

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <div className='flex flex-col gap-5 h-screen w-screen justify-center items-center'>
-        <img className="size-[20vh]"src={Logo}></img>
-        Coming Soon...
+        <img className="size-[20vh] "src={Logo}></img>
+        <p className='font-inter font-light'>Coming Soon...</p>
       </div>
     </>
   )
