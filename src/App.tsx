52f2295077f3634e678777a160bd29469aa60e3b
kk4w4i <kintarokawai@gmail.com>
2024-04-17 15:48:16 +1000

    zen jap setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Logo from './assets/Zen Japanese.webp'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col gap-5 h-screen w-screen justify-center items-center'>
+        <img className="size-[20vh]"src={Logo}></img>
+        Coming Soon...
+      </div>
+    </>
+  )
+}
+
+export default App
